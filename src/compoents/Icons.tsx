6739f8c7b84ce7ad76ecb28e6118503dfed90e8a
Jose Albizures <albizures3601@gmail.com>
2022-05-20 21:50:24 -0600

    feat(header): finish header

---
+++ src/compoents/Icons.tsx
@@ -0,0 +1,101 @@
+interface IconProps {
+	className?: string;
+	width?: string;
+	height?: string;
+}
+
+export function InstagramIcon(props: IconProps) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="20"
+			height="20"
+			viewBox="0 0 20 20"
+			{...props}
+		>
+			<g
+				id="Group_107"
+				data-name="Group 107"
+				transform="translate(-1208 -40)"
+			>
+				<circle
+					id="Ellipse_11"
+					data-name="Ellipse 11"
+					cx="10"
+					cy="10"
+					r="10"
+					transform="translate(1208 40)"
+					fill="#2b4f3f"
+				/>
+				<path
+					id="Icon_awesome-instagram"
+					data-name="Icon awesome-instagram"
+					d="M6,5.16A3.076,3.076,0,1,0,9.072,8.236,3.071,3.071,0,0,0,6,5.16Zm0,5.076a2,2,0,1,1,2-2,2,2,0,0,1-2,2Zm3.919-5.2A.717.717,0,1,1,9.2,4.317.716.716,0,0,1,9.915,5.035Zm2.037.728a3.551,3.551,0,0,0-.969-2.514A3.574,3.574,0,0,0,8.47,2.28c-.991-.056-3.959-.056-4.95,0a3.569,3.569,0,0,0-2.514.966A3.562,3.562,0,0,0,.037,5.76c-.056.991-.056,3.959,0,4.95a3.551,3.551,0,0,0,.969,2.514,3.578,3.578,0,0,0,2.514.969c.991.056,3.959.056,4.95,0a3.551,3.551,0,0,0,2.514-.969,3.574,3.574,0,0,0,.969-2.514c.056-.991.056-3.957,0-4.947Zm-1.28,6.01a2.025,2.025,0,0,1-1.14,1.14A13.222,13.222,0,0,1,6,13.154a13.325,13.325,0,0,1-3.536-.241,2.025,2.025,0,0,1-1.14-1.14,13.222,13.222,0,0,1-.241-3.536A13.325,13.325,0,0,1,1.319,4.7,2.025,2.025,0,0,1,2.46,3.56,13.222,13.222,0,0,1,6,3.319a13.325,13.325,0,0,1,3.536.241,2.025,2.025,0,0,1,1.14,1.14,13.222,13.222,0,0,1,.241,3.536A13.214,13.214,0,0,1,10.673,11.773Z"
+					transform="translate(1212.005 41.762)"
+					fill="#faf9f7"
+				/>
+			</g>
+		</svg>
+	);
+}
+
+export function FacebookIcon(props: IconProps) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="20"
+			height="19.879"
+			viewBox="0 0 20 19.879"
+			{...props}
+		>
+			<path
+				id="Icon_awesome-facebook"
+				data-name="Icon awesome-facebook"
+				d="M20.563,10.563A10,10,0,1,0,9,20.442V13.453H6.46V10.563H9v-2.2a3.528,3.528,0,0,1,3.777-3.89,15.39,15.39,0,0,1,2.239.2v2.46H13.754a1.445,1.445,0,0,0-1.629,1.562v1.877H14.9l-.444,2.891h-2.33v6.988A10,10,0,0,0,20.563,10.563Z"
+				transform="translate(-0.563 -0.563)"
+				fill="#2b4f3f"
+			/>
+		</svg>
+	);
+}
+
+export function LinkedinIcon(props: IconProps) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="20"
+			height="20"
+			viewBox="0 0 20 20"
+			{...props}
+		>
+			<g
+				id="Group_106"
+				data-name="Group 106"
+				transform="translate(-1267 -40)"
+			>
+				<circle
+					id="Ellipse_12"
+					data-name="Ellipse 12"
+					cx="10"
+					cy="10"
+					r="10"
+					transform="translate(1267 40)"
+					fill="#2b4f3f"
+				/>
+				<path
+					id="Icon_awesome-linkedin-in"
+					data-name="Icon awesome-linkedin-in"
+					d="M2.285,10.208H.169V3.393H2.285ZM1.226,2.463A1.231,1.231,0,1,1,2.451,1.226,1.236,1.236,0,0,1,1.226,2.463ZM10.2,10.208H8.093V6.89c0-.791-.016-1.8-1.1-1.8-1.1,0-1.269.859-1.269,1.747v3.374H3.61V3.393H5.64v.93h.03a2.224,2.224,0,0,1,2-1.1c2.142,0,2.535,1.41,2.535,3.242v3.743Z"
+					transform="translate(1272 44.999)"
+					fill="#faf9f7"
+				/>
+			</g>
+		</svg>
+	);
+}
+
+export const icons = {
+	instagram: InstagramIcon,
+	facebook: FacebookIcon,
+	linkedin: LinkedinIcon,
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,49 @@
 import Image from 'next/image';
+import clsx from 'clsx';
 import merge from 'lodash.merge';
 import { GetStaticProps } from 'next';
 import { LogoImg } from '../assets';
 import content from '../../content/content.json';
 import { useContent } from '../state';
+import { icons } from '../compoents/Icons';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+interface LanguageProps {
+	href: string;
+	locale: string;
+	children: React.ReactNode;
+}
+
+function Language(props: LanguageProps) {
+	const routers = useRouter();
+	const { href, locale, children } = props;
+
+	return (
+		<Link href={href} locale={locale}>
+			<a
+				className={clsx('text-flintstone text-sm', {
+					'text-hunt font-semibold': routers.locale === locale,
+				})}
+			>
+				{children}
+			</a>
+		</Link>
+	);
+}
 
 export default function Index() {
 	const content = useContent();
+	console.log(content);
+
 	return (
 		<>
 			<div className="shadow-sm">
-				<header className="flex max-w-3xl mx-auto py-4">
+				<header className="flex max-w-4xl mx-auto py-4">
 					<div className="h-12 w-48">
 						<Image src={LogoImg} layout="responsive" />
 					</div>
-					<nav className="self-center flex flex-1 mx-4">
+					<nav className="self-center flex flex-1 mx-6">
 						<ul className="flex justify-between content-center text-liquorice text-sm w-full">
 							{content.navbar.items.map((items) => {
 								return (
@@ -27,8 +56,34 @@ export default function Index() {
 							})}
 						</ul>
 					</nav>
-					<div className="flex content-center">
-						<div className="self-center">icons</div>
+					<div className="flex content-center space-x-3 items-center">
+						{content.navbar.socialMedia.map((icon, index) => {
+							const Icon = icons[icon.icon];
+							return (
+								<a
+									className="w-5 h-5 relative"
+									key={index}
+									href={icon.href}
+								>
+									<Icon
+										height="100%"
+										width="100%"
+										className="inset-0 absolute"
+									/>
+								</a>
+							);
+						})}
+						<div>
+							<Language href="/" locale="en">
+								Eng
+							</Language>
+							<span className="text-flintstone inline-block mx-1">
+								|
+							</span>
+							<Language href="/" locale="es">
+								Esp
+							</Language>
+						</div>
 					</div>
 				</header>
 			</div>
