b979ea3a0f808bcb08a6a5f05153b59b2f0feb6e
Jose Albizures <albizures3601@gmail.com>
2022-06-10 18:05:00 -0600

    fix: header for mobiles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
-import Image from 'next/image';
 import clsx from 'clsx';
-import { LogoImg } from '../assets';
-import { icons } from '../components/Icons';
+import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { icons, CloseIcon, MenuIcon } from '../components/Icons';
 import { useContent } from '../state';
+import { Logo2Img, LogoImg } from '../assets';
 
 interface LanguageProps {
 	href: string;
@@ -19,9 +20,12 @@ function Language(props: LanguageProps) {
 	return (
 		<Link href={href} locale={locale}>
 			<a
-				className={clsx('text-flintstone text-sm', {
-					'text-hunt font-semibold': routers.locale === locale,
-				})}
+				className={clsx(
+					'text-extra-white md:text-flintstone text-sm',
+					{
+						'md:text-hunt font-semibold': routers.locale === locale,
+					},
+				)}
 			>
 				{children}
 			</a>
@@ -31,18 +35,90 @@ function Language(props: LanguageProps) {
 
 export function Header() {
 	const { navbar } = useContent();
+	const [menuStatus, setMenuStatus] = React.useState<
+		'open' | 'close'
+	>('close');
+
+	React.useEffect(() => {
+		document.body.style.overflow =
+			menuStatus === 'open' ? 'hidden' : '';
+	}, [menuStatus]);
+
+	function onClickMenu() {
+		setMenuStatus((current) =>
+			current === 'open' ? 'close' : 'open',
+		);
+	}
+
+	function onCloseMenu() {
+		setMenuStatus('close');
+	}
+
 	return (
 		<div className="shadow-sm">
-			<header className="max-w-5xl mx-auto py-4 hidden md:flex">
-				<div className="h-12 w-48">
-					<Image src={LogoImg} layout="responsive" />
+			<header
+				className={clsx('max-w-5xl mx-auto py-4 md:justify-center', {
+					'bg-hunt fixed inset-0 z-30': menuStatus === 'open',
+					flex: menuStatus === 'close',
+				})}
+			>
+				<div
+					className={clsx(
+						'flex items-center justify-between w-full mx-6',
+						'md:(mx-0 w-auto)',
+						{
+							hidden: menuStatus === 'open',
+						},
+					)}
+				>
+					<button className="md:hidden -mr-3" onClick={onClickMenu}>
+						<MenuIcon />
+					</button>
+					<div className={clsx('h-12 w-48')}>
+						<Image src={LogoImg} layout="responsive" />
+					</div>
+					<span className="md:hidden">{/* space */}</span>
 				</div>
-				<nav className="self-center flex flex-1 mx-6">
-					<ul className="flex justify-between content-center text-liquorice text-sm w-full">
+
+				<div
+					className={clsx({
+						hidden: menuStatus === 'close',
+					})}
+				>
+					<div className="flex items-center justify-between w-full px-6">
+						<span>{/* space */}</span>
+						<div className={clsx('h-12 w-48')}>
+							<Image src={Logo2Img} layout="responsive" />
+						</div>
+						<button className="md:hidden -ml-3" onClick={onClickMenu}>
+							<CloseIcon className="text-extra-white text-2xl" />
+						</button>
+					</div>
+				</div>
+
+				<nav
+					className={clsx(
+						'self-center flex-1 mx-6 mt-14',
+						'md:(flex mt-0)',
+						{
+							hidden: menuStatus === 'close',
+						},
+					)}
+				>
+					<ul
+						className={clsx(
+							'flex flex-col text-xl space-y-6 text-extra-white justify-between content-center text-sm w-full',
+							'md:(flex-row flex-row text-liquorice text-sm space-y-0)',
+						)}
+					>
 						{navbar.items.map((items) => {
 							return (
 								<li key={items.href}>
-									<a className="hover:underline" href={items.href}>
+									<a
+										className="hover:underline"
+										onClick={onCloseMenu}
+										href={items.href}
+									>
 										{items.label}
 									</a>
 								</li>
@@ -50,28 +126,39 @@ export function Header() {
 						})}
 					</ul>
 				</nav>
-				<div className="flex content-center space-x-3 items-center">
-					{navbar.socialMedia.map((icon, index) => {
-						const Icon = icons[icon.icon];
-						return (
-							<a
-								className="w-5 h-5 relative"
-								key={index}
-								href={icon.href}
-							>
-								<Icon
-									height="100%"
-									width="100%"
-									className="inset-0 absolute"
-								/>
-							</a>
-						);
-					})}
-					<div>
+				<div
+					className={clsx(
+						'flex absolute bottom-0 w-full p-6 content-center space-x-3 items-center justify-between',
+						'md:(flex relative p-0 w-auto)',
+						{
+							hidden: menuStatus === 'close',
+						},
+					)}
+				>
+					<div className="flex space-x-2">
+						{navbar.socialMedia.map((icon, index) => {
+							const Icon = icons[icon.icon];
+							return (
+								<a
+									className="w-5 h-5 relative"
+									key={index}
+									href={icon.href}
+								>
+									<Icon
+										height="100%"
+										width="100%"
+										className="inset-0 absolut text-extra-white md:text-hunt w-5 h-5"
+										pathClassName="fill-hunt md:fill-white"
+									/>
+								</a>
+							);
+						})}
+					</div>
+					<div className="flex md:items-center">
 						<Language href="/" locale="en">
 							Eng
 						</Language>
-						<span className="text-flintstone inline-block mx-1">
+						<span className="text-extra-white md:text-flintstone inline-block mx-1">
 							|
 						</span>
 						<Language href="/" locale="es">
--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -4,90 +4,79 @@ interface IconProps {
 	height?: string;
 }
 
-export function InstagramIcon(props: IconProps) {
+interface CircleIconProps extends IconProps {
+	pathClassName: string;
+}
+
+export function InstagramIcon(props: CircleIconProps) {
+	const { pathClassName, ...more } = props;
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			width="20"
 			height="20"
 			viewBox="0 0 20 20"
-			{...props}
+			{...more}
 		>
-			<g
-				id="Group_107"
-				data-name="Group 107"
-				transform="translate(-1208 -40)"
-			>
+			<g transform="translate(-1208 -40)">
 				<circle
-					id="Ellipse_11"
-					data-name="Ellipse 11"
 					cx="10"
 					cy="10"
 					r="10"
 					transform="translate(1208 40)"
-					fill="#2b4f3f"
+					fill="currentColor"
 				/>
 				<path
-					id="Icon_awesome-instagram"
-					data-name="Icon awesome-instagram"
 					d="M6,5.16A3.076,3.076,0,1,0,9.072,8.236,3.071,3.071,0,0,0,6,5.16Zm0,5.076a2,2,0,1,1,2-2,2,2,0,0,1-2,2Zm3.919-5.2A.717.717,0,1,1,9.2,4.317.716.716,0,0,1,9.915,5.035Zm2.037.728a3.551,3.551,0,0,0-.969-2.514A3.574,3.574,0,0,0,8.47,2.28c-.991-.056-3.959-.056-4.95,0a3.569,3.569,0,0,0-2.514.966A3.562,3.562,0,0,0,.037,5.76c-.056.991-.056,3.959,0,4.95a3.551,3.551,0,0,0,.969,2.514,3.578,3.578,0,0,0,2.514.969c.991.056,3.959.056,4.95,0a3.551,3.551,0,0,0,2.514-.969,3.574,3.574,0,0,0,.969-2.514c.056-.991.056-3.957,0-4.947Zm-1.28,6.01a2.025,2.025,0,0,1-1.14,1.14A13.222,13.222,0,0,1,6,13.154a13.325,13.325,0,0,1-3.536-.241,2.025,2.025,0,0,1-1.14-1.14,13.222,13.222,0,0,1-.241-3.536A13.325,13.325,0,0,1,1.319,4.7,2.025,2.025,0,0,1,2.46,3.56,13.222,13.222,0,0,1,6,3.319a13.325,13.325,0,0,1,3.536.241,2.025,2.025,0,0,1,1.14,1.14,13.222,13.222,0,0,1,.241,3.536A13.214,13.214,0,0,1,10.673,11.773Z"
 					transform="translate(1212.005 41.762)"
-					fill="#faf9f7"
+					className={pathClassName}
 				/>
 			</g>
 		</svg>
 	);
 }
 
-export function FacebookIcon(props: IconProps) {
+export function FacebookIcon(props: CircleIconProps) {
+	const { pathClassName, ...more } = props;
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			width="20"
 			height="19.879"
 			viewBox="0 0 20 19.879"
-			{...props}
+			{...more}
 		>
 			<path
-				id="Icon_awesome-facebook"
-				data-name="Icon awesome-facebook"
 				d="M20.563,10.563A10,10,0,1,0,9,20.442V13.453H6.46V10.563H9v-2.2a3.528,3.528,0,0,1,3.777-3.89,15.39,15.39,0,0,1,2.239.2v2.46H13.754a1.445,1.445,0,0,0-1.629,1.562v1.877H14.9l-.444,2.891h-2.33v6.988A10,10,0,0,0,20.563,10.563Z"
 				transform="translate(-0.563 -0.563)"
-				fill="#2b4f3f"
+				fill="currentColor"
 			/>
 		</svg>
 	);
 }
 
-export function LinkedinIcon(props: IconProps) {
+export function LinkedinIcon(props: CircleIconProps) {
+	const { pathClassName, ...more } = props;
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			width="20"
 			height="20"
 			viewBox="0 0 20 20"
-			{...props}
+			{...more}
 		>
-			<g
-				id="Group_106"
-				data-name="Group 106"
-				transform="translate(-1267 -40)"
-			>
+			<g transform="translate(-1267 -40)">
 				<circle
-					id="Ellipse_12"
-					data-name="Ellipse 12"
 					cx="10"
 					cy="10"
 					r="10"
 					transform="translate(1267 40)"
-					fill="#2b4f3f"
+					fill="currentColor"
 				/>
 				<path
-					id="Icon_awesome-linkedin-in"
-					data-name="Icon awesome-linkedin-in"
 					d="M2.285,10.208H.169V3.393H2.285ZM1.226,2.463A1.231,1.231,0,1,1,2.451,1.226,1.236,1.236,0,0,1,1.226,2.463ZM10.2,10.208H8.093V6.89c0-.791-.016-1.8-1.1-1.8-1.1,0-1.269.859-1.269,1.747v3.374H3.61V3.393H5.64v.93h.03a2.224,2.224,0,0,1,2-1.1c2.142,0,2.535,1.41,2.535,3.242v3.743Z"
 					transform="translate(1272 44.999)"
-					fill="#faf9f7"
+					className={pathClassName}
 				/>
 			</g>
 		</svg>
@@ -103,11 +92,7 @@ export function RightArrowIcon(props: IconProps) {
 			viewBox="0 0 68 68"
 			{...props}
 		>
-			<g
-				id="Icon_feather-arrow-right-circle"
-				data-name="Icon feather-arrow-right-circle"
-				transform="translate(-1.5 -1.5)"
-			>
+			<g transform="translate(-1.5 -1.5)">
 				<path
 					id="Path_1308"
 					data-name="Path 1308"
@@ -299,7 +284,7 @@ export function PhoneIcon(props: IconProps) {
 			<g
 				id="Group_96"
 				transform="translate(0 0)"
-				clip-path="url(#clip-path)"
+				clipPath="url(#clip-path)"
 			>
 				<path
 					id="Path_550"
@@ -333,7 +318,7 @@ export function EmailIcon(props: IconProps) {
 				</clipPath>
 			</defs>
 			<g transform="translate(6)">
-				<g transform="translate(-6 0)" clip-path="url(#clip-path)">
+				<g transform="translate(-6 0)" clipPath="url(#clip-path)">
 					<path
 						d="M15.758,20.706,2.675,14.565a4.652,4.652,0,0,1,0-8.423L15.758,0,29.046,5.831a4.938,4.938,0,0,1,0,9.043Z"
 						transform="translate(0 0)"
@@ -369,7 +354,7 @@ export function AddressIcon(props: IconProps) {
 					<rect width="32" height="20.706" fill="none" />
 				</clipPath>
 			</defs>
-			<g transform="translate(0 0)" clip-path="url(#clip-path)">
+			<g transform="translate(0 0)" clipPath="url(#clip-path)">
 				<path
 					d="M15.758,20.706,2.675,14.565a4.652,4.652,0,0,1,0-8.423L15.758,0,29.046,5.831a4.938,4.938,0,0,1,0,9.043Z"
 					transform="translate(0 0)"
@@ -381,6 +366,44 @@ export function AddressIcon(props: IconProps) {
 					fill="#fff"
 				/>
 			</g>
+		</svg>
+	);
+}
+
+export function MenuIcon(props: IconProps) {
+	return (
+		<svg
+			stroke="currentColor"
+			fill="none"
+			strokeWidth="2"
+			viewBox="0 0 24 24"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			height="1em"
+			width="1em"
+			xmlns="http://www.w3.org/2000/svg"
+			{...props}
+		>
+			<line x1="3" y1="12" x2="21" y2="12"></line>
+			<line x1="3" y1="6" x2="21" y2="6"></line>
+			<line x1="3" y1="18" x2="21" y2="18"></line>
+		</svg>
+	);
+}
+
+export function CloseIcon(props: IconProps) {
+	return (
+		<svg
+			stroke="currentColor"
+			fill="currentColor"
+			strokeWidth="0"
+			viewBox="0 0 512 512"
+			height="1em"
+			width="1em"
+			xmlns="http://www.w3.org/2000/svg"
+			{...props}
+		>
+			<path d="M289.94 256l95-95A24 24 0 00351 127l-95 95-95-95a24 24 0 00-34 34l95 95-95 95a24 24 0 1034 34l95-95 95 95a24 24 0 0034-34z"></path>
 		</svg>
 	);
 }
