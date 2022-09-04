4c5ceee6e3278b7b9d0e1afa896980addb2ccc74
Jose Albizures <albizures3601@gmail.com>
2022-09-04 17:35:50 -0600

    feat: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,7 @@ export default function Document() {
 					href="https://fonts.gstatic.com"
 					crossOrigin="true"
 				/>
+				<link rel="icon" href="favicon.svg"></link>
 				<link
 					href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Work+Sans:wght@400;500;600;700&display=swap"
 					rel="stylesheet"
